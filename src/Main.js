f5bb6656fe006145ba854047695c2135ae65d45c
lamouzsafouen <lamouzsafouen@gmail.com>
2021-01-23 13:23:45 +0100

    Create a React app and publish it to GitHub Pages

--- src/Main.js
+++ src/Main.js
@@ -14,7 +14,7 @@ class Main extends Component {
         return (
             <HashRouter>
                 <div>
-                    <h1>Simple SPA</h1>
+                    <h1>Project React</h1>
                     <ul className="header">
                         <li><NavLink exact to="/">Home</NavLink></li>
                         <li><NavLink to="/stuff">Stuff</NavLink></li>
